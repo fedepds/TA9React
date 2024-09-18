a152f05bf17efa222b257f98faafd2ac6b76fa9a
Federico Pizarro <127800142+fedepds@users.noreply.github.com>
2024-09-18 18:07:40 -0300

    comenzar TA9

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+let Buttons = () => {
+  const [count, setCount] = useState(0);
+
+  function incrementarClick() {
+    setCount((prevValue)=>prevValue + 1);
+  }
+  function decrementarClick() {
+    if(count>0){
+      setCount((prevValue)=>prevValue -1);
+    }
+    else{
+      setCount(0)
+    }
+   
+  }
+  function resetClick() {
+    setCount(0);
+  }
+
+  return (
+    <>
+      <div className='contador'>
+        <p>{count}</p> 
+      </div>
+
+      <div className='botones'>
+        <button onClick={incrementarClick}>
+          Aumentar
+        </button>
+        <button onClick={decrementarClick}>
+          Decrementar
+        </button>
+        <button onClick={resetClick}>
+          Restablecer
+        </button>
+      </div>
+    </>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Buttons />
     </div>
   );
 }
